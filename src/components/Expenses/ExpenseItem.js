e5a32278b3ad53f1ddeb7c9202044a074aa3090f
rishonkumar <rishon.kumar.rahi@gmail.com>
2022-08-25 09:37:57 +0530

    UseState Learning

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,27 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+import { useState } from "react";
+
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    // console.log("Clicked");
+    setTitle("Updated");
+  };
+
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+      {/* <button onClick={() => console.log("Clicked")}>Change Title</button> */}
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
   );
 }
 
